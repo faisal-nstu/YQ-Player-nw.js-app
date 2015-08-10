7a9f8459f7129243f7c93a888da44033aca6a054
Faisal Amin <faisal.nstu@ymail.com>
2015-08-10 18:57:18 +0600

    playlist collapse feature

--- jsbin_files/player-iframe.js
+++ jsbin_files/player-iframe.js
@@ -1,190 +1,214 @@
-    var tag;
-    var done = false;
-    var index = 0;
-    var urlList = [];
-    var items;
-    var player;
+var tag;
+var done = false;
+var index = 0;
+var urlList = [];
+//var urlList = ["bHQqvYy5KYo"];
+var items;
+var player;
 
-    // This code loads the IFrame Player API code asynchronously.
-    function createPlayer() {
-      tag = document.createElement('script');
+// 2. This code loads the IFrame Player API code asynchronously.
+function createPlayer() {
+  tag = document.createElement('script');
 
-      tag.src = "https://www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName('script')[0];
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
 
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+}
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    width: '100%',
+    height: 320,
+    videoId: GetUrl(index),
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
     }
+  });
+}
 
-    //  This function creates an <iframe> (and YouTube player)
-    //  after the API code downloads.
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
 
-    function onYouTubeIframeAPIReady() {
-      player = new YT.Player('player', {
-        width: '100%',
-        height: 320,
-        videoId: GetUrl(index),
-        events: {
-          'onReady': onPlayerReady,
-          'onStateChange': onPlayerStateChange
-        }
-      });
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+
+
+function GetUrl(index) {
+  done = false;
+  return urlList[index];
+}
+
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    //setTimeout(stopVideo, 9000);
+    done = true;
+  }
+  //'bHQqvYy5KYo'
+  if (event.data === YT.PlayerState.ENDED && done === true) {
+    index++;
+    if (index >= urlList.length) {
+    index = 0;
+  }
+  PlayAnother(GetUrl(index));
+  
+  }
+}
+
+function PlayAnother(urll) {
+  player.loadVideoById(urll, 0, "large");
+  player.playVideo();
+}
+
+function stopVideo() {
+  player.stopVideo();
+}
+
+// get url from form
+function addClick() {
+  var x = document.getElementById("frm1");
+
+  var text = "";
+  //var i;
+  for (var i = 0; i < x.length; i++) {
+    text += x.elements[i].value;
+  }
+
+  var vidId = youtube_parser(text);
+  //var res = text.split("?v=");
+  //var id = res[1].split("&");
+
+
+  //console.log(id[0]);
+  if (vidId !== undefined) {
+    urlList.push(vidId);
+    addToPlayList(vidId);
+    if(tag === undefined){
+      createPlayer();
     }
+  }
+  x.reset();
 
-    // The API will call this function when the video player is ready.
-    function onPlayerReady(event) {
-      event.target.playVideo();
-    }
+}
 
-    function GetUrl(index) {
-      done = false;
-      return urlList[index];
-    }
+function youtube_parser(url) {
+  var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+  var match = url.match(regExp);
+  if (match && match[7].length == 11) {
+    return match[7];
+  } else {
+    alert("Wrong URL");
+  }
+}
 
-    // The API calls this function when the player's state changes.
-    // The function indicates that when playing a video (state=1),
-    // the player should play for six seconds and then stop.
+function playNext() {
+  player.stopVideo();
+  if (index >= urlList.length) {
+    index = 0;
+  }
+  PlayAnother(GetUrl(index));
+  index++;
+}
 
-    function onPlayerStateChange(event) {
-      if (event.data == YT.PlayerState.PLAYING && !done) {
-        done = true;
-      }
-      if (event.data === YT.PlayerState.ENDED && done === true) {
-        index++;
-        if (index >= urlList.length) {
-          index = 0;
-        }
-        PlayAnother(GetUrl(index));
-      }
-    }
+function playPrev() {
+  player.stopVideo();
+  index = index--;
+  if (index < 0) {
+    index = urlList.length - 1;
+  }
+  PlayAnother(GetUrl(index));
+}
 
-    function PlayAnother(urll) {
-      player.loadVideoById(urll, 0, "large");
-      player.playVideo();
-    }
+var addToPlayList = function addToList(id) {
+  var node = document.createElement("LI");
+  var track = document.createTextNode(id);
+  node.appendChild(track);
+  document.getElementById("playList").appendChild(node);
+  items = document.querySelectorAll('li');
+  [].forEach.call(items, function(item) {
+    item.setAttribute('draggable', true);
+    item.addEventListener('dragstart', dragstart, false);
+    item.addEventListener('dragenter', dragenter, false);
+  });
+};
 
-    function stopVideo() {
-      player.stopVideo();
-    }
+var pl = document.getElementById('playList');
+pl.onclick = function(clickedItem) {
+  var target = getEventTarget(clickedItem);
+  var vid = target.innerHTML;
+  PlayAnother(vid);
+};
 
-    function addClick() {
-      // get url from form
-      var x = document.getElementById("frm1");
+function getEventTarget(e) {
+  e = e || window.event;
+  return e.target || e.srcElement;
+}
 
-      var text = "";
-      for (var i = 0; i < x.length; i++) {
-        text += x.elements[i].value;
-      }
+// ======================================
+// drag N drop
 
-      var vidId = youtube_parser(text);
+var source;
 
-      if (vidId !== undefined) {
-        urlList.push(vidId);
-        addToPlayList(vidId);
-        if(tag === undefined){
-          createPlayer();
-        }
-      }
-      x.reset();
-    }
-    // ==================================
-    // GEXTRACT VIDEO ID FROM URL START
-    function youtube_parser(url) {
-      var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
-      var match = url.match(regExp);
-      if (match && match[7].length == 11) {
-        return match[7];
-      } else {
-        alert("Wrong URL");
-      }
-    }
-
-    // EXTRACT VIDEO ID FROM URL END
-    // =================================
-
-    function playNext() {
-      player.stopVideo();
-      if (index >= urlList.length) {
-        index = 0;
-      }
-      PlayAnother(GetUrl(index));
-      index++;
-    }
-
-    function playPrev() {
-      player.stopVideo();
-      index = index--;
-      if (index < 0) {
-        index = urlList.length - 1;
-      }
-      PlayAnother(GetUrl(index));
-    }
-
-    var addToPlayList = function addToList(id) {
-      var node = document.createElement("LI");
-      var track = document.createTextNode(id);
-      node.appendChild(track);
-      document.getElementById("playList").appendChild(node);
-      items = document.querySelectorAll('li');
-      [].forEach.call(items, function(item) {
-        item.setAttribute('draggable', true);
-        item.addEventListener('dragstart', dragstart, false);
-        item.addEventListener('dragenter', dragenter, false);
-      });
-    };
-
-    var pl = document.getElementById('playList');
-    pl.onclick = function(clickedItem) {
-      var target = getEventTarget(clickedItem);
-      var vid = target.innerHTML;
-      PlayAnother(vid);
-    };
-
-    function getEventTarget(e) {
-      e = e || window.event;
-      return e.target || e.srcElement;
-    }
-
-    // ======================================
-    // DRAG AND DROP START
-
-    var source;
-
-    function isbefore(a, b) {
-      if (a.parentNode == b.parentNode) {
-        for (var cur = a; cur; cur = cur.previousSibling) {
-          if (cur === b) {
-            return true;
-          }
-        }
-      }
-      return false;
-    }
-
-    function dragenter(e) {
-      if (isbefore(source, e.target)) {
-        e.target.parentNode.insertBefore(source, e.target);
-      } else {
-        e.target.parentNode.insertBefore(source, e.target.nextSibling);
+function isbefore(a, b) {
+  if (a.parentNode == b.parentNode) {
+    for (var cur = a; cur; cur = cur.previousSibling) {
+      if (cur === b) {
+        return true;
       }
     }
+  }
+  return false;
+}
 
-    function dragstart(e) {
-      source = e.target;
-      e.dataTransfer.effectAllowed = 'move';
-    }
-      // DRAG AND DROP END
-      //=============================================
+function dragenter(e) {
+  if (isbefore(source, e.target)) {
+    e.target.parentNode.insertBefore(source, e.target);
+  } else {
+    e.target.parentNode.insertBefore(source, e.target.nextSibling);
+  }
+}
 
-    // ===============================================
-    // HIDE VIDEO START
+function dragstart(e) {
+    source = e.target;
+    e.dataTransfer.effectAllowed = 'move';
+  }
+  // drag N drop
+  //=============================================
 
-    function hideVideo() {
-      var e = document.getElementById("hider_div");
-      if (e.style.display == 'block')
-        e.style.display = 'none';
-      else
-        e.style.display = 'block';
-    }
+// ===============================================
+// HIDE VIDEO
 
-    // HIDE VIDEO END
-    // ==================================================
+function hideVideo() {
+  var e = document.getElementById("hider_div");
+  if (e.style.display == 'block')
+    e.style.display = 'none';
+  else
+    e.style.display = 'block';
+}
+//==========================================================
+// TOGGLE PLAYLIST
+function togglePlaylist(){
+  
+  var pl = document.getElementById('playlist_cont');
+  if(pl.style.display === 'block'){
+    pl.style.display = 'none';
+    parent.postMessage("hide-playlist", "*");
+  }
+  else{
+    pl.style.display = 'block';
+    parent.postMessage("show-playlist", "*");
+  }
+}
+
+
+// https://www.youtube.com/watch?v=bHQqvYy5KYo
+// https://www.youtube.com/watch?v=TugL2HTrUPw
+// https://www.youtube.com/watch?v=4osCnNbq8Hc
+// http://i.imgur.com/2E7zcyM.gif
